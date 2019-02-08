fa62bf94874e67ade72c486ee19df2e8d2c44543
karimdaou <elhkari@LAPTOP-PNJQK2JS.localdomain>
2019-02-09 00:37:41 +0300

    added terms at the cut

--- masters.c
+++ masters.c
@@ -61,7 +61,7 @@ void prepare_functions() {
                 V[i][j][k] = -1. / (sqrt((x[i] + a / 2.) * (x[i] + a / 2.) + y[j] * y[j] + z[k] * z[k])) - \
                                 1. / (sqrt((x[i] - a / 2.) * (x[i] - a / 2.) + y[j] * y[j] + z[k] * z[k]));
 
-                psi[i][j][k] = exp(-sqrt((x[i] - a/2) * (x[i] - a/2) + y[j] * y[j] + z[k] * z[k]));
+                psi[i][j][k] = exp(-sqrt((x[i] - a / 2) * (x[i] - a / 2) + y[j] * y[j] + z[k] * z[k]));
                 vel[i][j][k] = 0;
             }
 
@@ -158,16 +158,101 @@ void calculate_E() {
         }
     }
 
+
+    // Члены на границе разреза
+    i = 0;
+    for (j = 0; j < NUM - 1; j++) {
+        //поверхностные члены (входят в числитель функционала энергии)
+        // (из слагаемого с множителем L, отвечающего за ГУ)
+        sum21 += psi[i][j][M] * psi[i][j][M] * dx[i] * dy[j];
+        sum23 += psi[i][j + 1][M] * psi[i][j + 1][M] * dx[i] * dy[j];
+
+        for (k = M; k < NUM2 - 1; k++) {
+
+            //слагаемые, входящие в числитель функционала энергии
+            sum5 += (psi[i][j + 1][k] - psi[i][j][k]) * (psi[i][j + 1][k] - psi[i][j][k]) * dx[i] * dz[k] / dy[j];
+            sum7 += (psi[i][j + 1][k + 1] - psi[i][j][k + 1]) * \
+                            (psi[i][j + 1][k + 1] - psi[i][j][k + 1]) * dx[i] * dz[k] / dy[j];
+
+            sum9 += (psi[i][j][k + 1] - psi[i][j][k]) * (psi[i][j][k + 1] - psi[i][j][k]) * dx[i] * dy[j] / dz[k];
+            sum11 += (psi[i][j + 1][k + 1] - psi[i][j + 1][k]) * \
+                            (psi[i][j + 1][k + 1] - psi[i][j + 1][k]) * dx[i] * dy[j] / dz[k];
+
+            sum13 += V[i][j][k] * psi[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+            sum15 += V[i][j + 1][k] * psi[i][j + 1][k] * psi[i][j + 1][k] * dx[i] * dy[j] * dz[k];
+            sum16 += V[i][j][k + 1] * psi[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+            sum19 += V[i][j + 1][k + 1] * psi[i][j + 1][k + 1] * psi[i][j + 1][k + 1] * dx[i] * dy[j] * dz[k];
+
+            //слагаемые, входящие в знаменатель функционала энергии
+            sum25 += psi[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+            sum27 += psi[i][j + 1][k] * psi[i][j + 1][k] * dx[i] * dy[j] * dz[k];
+            sum28 += psi[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+            sum31 += psi[i][j + 1][k + 1] * psi[i][j + 1][k + 1] * dx[i] * dy[j] * dz[k];
+        }
+    }
+
+
+    j = 0;
+    for (i = 0; i < NUM - 1; i++) {
+        //поверхностные члены (входят в числитель функционала энергии)
+        // (из слагаемого с множителем L, отвечающего за ГУ)
+        sum21 += psi[i][j][M] * psi[i][j][M] * dx[i] * dy[j];
+        sum22 += psi[i + 1][j][M] * psi[i + 1][j][M] * dx[i] * dy[j];
+
+        for (k = M; k < NUM2 - 1; k++) {
+
+            //слагаемые, входящие в числитель функционала энергии
+            sum1 += (psi[i + 1][j][k] - psi[i][j][k]) * (psi[i + 1][j][k] - psi[i][j][k]) * dy[j] * dz[k] / dx[i];
+            sum3 += (psi[i + 1][j][k + 1] - psi[i][j][k + 1]) * \
+                            (psi[i + 1][j][k + 1] - psi[i][j][k + 1]) * dy[j] * dz[k] / dx[i];
+
+            sum9 += (psi[i][j][k + 1] - psi[i][j][k]) * (psi[i][j][k + 1] - psi[i][j][k]) * dx[i] * dy[j] / dz[k];
+            sum10 += (psi[i + 1][j][k + 1] - psi[i + 1][j][k]) * \
+                            (psi[i + 1][j][k + 1] - psi[i + 1][j][k]) * dx[i] * dy[j] / dz[k];
+
+            sum13 += V[i][j][k] * psi[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+            sum14 += V[i + 1][j][k] * psi[i + 1][j][k] * psi[i + 1][j][k] * dx[i] * dy[j] * dz[k];
+            sum16 += V[i][j][k + 1] * psi[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+            sum18 += V[i + 1][j][k + 1] * psi[i + 1][j][k + 1] * psi[i + 1][j][k + 1] * dx[i] * dy[j] * dz[k];
+
+            //слагаемые, входящие в знаменатель функционала энергии
+            sum25 += psi[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+            sum26 += psi[i + 1][j][k] * psi[i + 1][j][k] * dx[i] * dy[j] * dz[k];
+            sum28 += psi[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+            sum30 += psi[i + 1][j][k + 1] * psi[i + 1][j][k + 1] * dx[i] * dy[j] * dz[k];
+        }
+    }
+
+
+    j = 0;
+    i = 0;
+    //поверхностные члены (входят в числитель функционала энергии)
+    // (из слагаемого с множителем L, отвечающего за ГУ)
+    sum21 += psi[i][j][M] * psi[i][j][M] * dx[i] * dy[j];
+
+    for (k = M; k < NUM2 - 1; k++) {
+
+        //слагаемые, входящие в числитель функционала энергии
+        sum9 += (psi[i][j][k + 1] - psi[i][j][k]) * (psi[i][j][k + 1] - psi[i][j][k]) * dx[i] * dy[j] / dz[k];
+
+        sum13 += V[i][j][k] * psi[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+        sum16 += V[i][j][k + 1] * psi[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+
+        //слагаемые, входящие в знаменатель функционала энергии
+        sum25 += psi[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+        sum28 += psi[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+    }
+
+
     E_kinetic = (sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 \
  + sum9 + sum10 + sum11 + sum12) / 8.;
     E_potential = (sum13 + sum14 + sum15 + sum16 + sum17 + sum18 \
  + sum19 + sum20) / 8.;
-    surface_term = L*(sum21 + sum22 + sum23 + sum24) / 8.;
+    surface_term = L * (sum21 + sum22 + sum23 + sum24) / 8.;
     numerator = E_kinetic + E_potential + surface_term;
     denumerator = (sum25 + sum26 + sum27 + sum28 + sum29 + sum30 + sum31 + sum32) / 8.;
 
     E = numerator / denumerator;
-
 }
 
 void clear_grad_E() {
@@ -334,17 +419,192 @@ void calculate_grad_E() {
                 // += (i+1, j+1, k+1)
                 d_w8 = psi[i + 1][j + 1][k + 1] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
                 grad_E[i + 1][j + 1][k + 1] += d_w8;
-
             }
         }
+    }
+
+
+    i = 0;
+    for (j = 0; j < NUM - 1; j++) {
+        grad_E[i][j][M] -= L * psi[i][j][M] * dx[i] * dy[j];
+        grad_E[i][j + 1][M] -= L * psi[i][j + 1][M] * dx[i] * dy[j];
+
+        for (k = M; k < NUM2 - 1; k++) {
+
+            //слагаемые, входящие в производную числителя функционала энергии
+
+            //КИНЕТИЧЕСКИЕ СЛАГАЕМЫЕ
+            // += (i, j+1, k) // -= (i, j, k)
+            d_v5 = (psi[i][j + 1][k] - psi[i][j][k]) * dx[i] * dz[k] / dy[j];
+            grad_E[i][j + 1][k] -= d_v5;
+            grad_E[i][j][k] += d_v5;
+
+            // += (i, j+1, k+1) // -= (i, j, k+1)
+            d_v7 = (psi[i][j + 1][k + 1] - psi[i][j][k + 1]) * dx[i] * dz[k] / dy[j];
+            grad_E[i][j + 1][k + 1] -= d_v7;
+            grad_E[i][j][k + 1] += d_v7;
+
+/////////////////////////////////////////////////////////////////
+            // += (i, j, k+1) // -= (i, j, k)
+            d_v9 = (psi[i][j][k + 1] - psi[i][j][k]) * dx[i] * dy[j] / dz[k];
+            grad_E[i][j][k + 1] -= d_v9;
+            grad_E[i][j][k] += d_v9;
+
+            // += (i, j+1, k+1) // -= (i, j+1, k)
+            d_v11 = (psi[i][j + 1][k + 1] - psi[i][j + 1][k]) * dx[i] * dy[j] / dz[k];
+            grad_E[i][j + 1][k + 1] -= d_v11;
+            grad_E[i][j + 1][k] += d_v11;
+
+////////////////////////////////////////////////////////////////
+            //ПОТЕНЦИАЛЬНЫЕ СЛАГАЕМЫЕ
+            // += (i, j, k)
+            d_v13 = V[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+            grad_E[i][j][k] -= d_v13;
+
+            // += (i, j+1, k)
+            d_v15 = V[i][j + 1][k] * psi[i][j + 1][k] * dx[i] * dy[j] * dz[k];
+            grad_E[i][j + 1][k] -= d_v15;
+
+            // += (i, j, k+1)
+            d_v16 = V[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+            grad_E[i][j][k + 1] -= d_v16;
+
+            // += (i, j+1, k+1)
+            d_v19 = V[i][j + 1][k + 1] * psi[i][j + 1][k + 1] * dx[i] * dy[j] * dz[k];
+            grad_E[i][j + 1][k + 1] -= d_v19;
+
+////////////////////////////////////////////////////////////////
+            //слагаемые, входящие в производную знаменателя функционала энергии
+            // += (i, j, k)
+            d_w1 = psi[i][j][k] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i][j][k] += d_w1;
+
+            // += (i, j+1, k)
+            d_w3 = psi[i][j + 1][k] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i][j + 1][k] += d_w3;
+
+            // += (i, j, k+1)
+            d_w4 = psi[i][j][k + 1] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i][j][k + 1] += d_w4;
+
+            // += (i, j+1, k+1)
+            d_w7 = psi[i][j + 1][k + 1] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i][j + 1][k + 1] += d_w7;
+        }
+    }
+
+
+    j = 0;
+    for (i = 0; i < NUM - 1; i++) {
+        grad_E[i][j][M] -= L * psi[i][j][M] * dx[i] * dy[j];
+        grad_E[i + 1][j][M] -= L * psi[i + 1][j][M] * dx[i] * dy[j];
+
+        for (k = M; k < NUM2 - 1; k++) {
+
+            //слагаемые, входящие в производную числителя функционала энергии
+
+            //КИНЕТИЧЕСКИЕ СЛАГАЕМЫЕ
+            // += (i+1, j, k) // -= (i, j, k)
+            d_v1 = (psi[i + 1][j][k] - psi[i][j][k]) * dy[j] * dz[k] / dx[i];;
+            //помним, что записыываем (-1)*(градиент энергии)
+            grad_E[i + 1][j][k] -= d_v1;
+            grad_E[i][j][k] += d_v1;
+
+            // += (i+1, j, k+1) // -= (i, j, k+1)
+            d_v3 = (psi[i + 1][j][k + 1] - psi[i][j][k + 1]) * dy[j] * dz[k] / dx[i];
+            grad_E[i + 1][j][k + 1] -= d_v3;
+            grad_E[i][j][k + 1] += d_v3;
+
+////////////////////////////////////////////////////////////////
+            // += (i, j, k+1) // -= (i, j, k)
+            d_v9 = (psi[i][j][k + 1] - psi[i][j][k]) * dx[i] * dy[j] / dz[k];
+            grad_E[i][j][k + 1] -= d_v9;
+            grad_E[i][j][k] += d_v9;
+
+            // += (i+1, j, k+1) // -= (i+1, j, k)
+            d_v10 = (psi[i + 1][j][k + 1] - psi[i + 1][j][k]) * dx[i] * dy[j] / dz[k];
+            grad_E[i + 1][j][k + 1] -= d_v10;
+            grad_E[i + 1][j][k] += d_v10;
+
+////////////////////////////////////////////////////////////////
+            //ПОТЕНЦИАЛЬНЫЕ СЛАГАЕМЫЕ
+            // += (i, j, k)
+            d_v13 = V[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+            grad_E[i][j][k] -= d_v13;
+
+            // += (i+1, j, k)
+            d_v14 = V[i + 1][j][k] * psi[i + 1][j][k] * dx[i] * dy[j] * dz[k];
+            grad_E[i + 1][j][k] -= d_v14;
+
+            // += (i, j, k+1)
+            d_v16 = V[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+            grad_E[i][j][k + 1] -= d_v16;
+
+            // += (i+1, j, k+1)
+            d_v18 = V[i + 1][j][k + 1] * psi[i + 1][j][k + 1] * dx[i] * dy[j] * dz[k];
+            grad_E[i + 1][j][k + 1] -= d_v18;
+
+////////////////////////////////////////////////////////////////
+            //слагаемые, входящие в производную знаменателя функционала энергии
+            // += (i, j, k)
+            d_w1 = psi[i][j][k] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i][j][k] += d_w1;
+
+            // += (i+1, j, k)
+            d_w2 = psi[i + 1][j][k] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i + 1][j][k] += d_w2;
+
+            // += (i, j, k+1)
+            d_w4 = psi[i][j][k + 1] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i][j][k + 1] += d_w4;
+
+            // += (i+1, j, k+1)
+            d_w6 = psi[i + 1][j][k + 1] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+            grad_E[i + 1][j][k + 1] += d_w6;
+        }
+    }
+
+
+    i = 0;
+    j = 0;
+    grad_E[i][j][M] -= L * psi[i][j][M] * dx[i] * dy[j];
+
+    for (k = M; k < NUM2 - 1; k++) {
+
+        //слагаемые, входящие в производную числителя функционала энергии
+
+        //КИНЕТИЧЕСКИЕ СЛАГАЕМЫЕ
+
+        // += (i, j, k+1) // -= (i, j, k)
+        d_v9 = (psi[i][j][k + 1] - psi[i][j][k]) * dx[i] * dy[j] / dz[k];
+        grad_E[i][j][k + 1] -= d_v9;
+        grad_E[i][j][k] += d_v9;
+
+        //ПОТЕНЦИАЛЬНЫЕ СЛАГАЕМЫЕ
+        // += (i, j, k)
+        d_v13 = V[i][j][k] * psi[i][j][k] * dx[i] * dy[j] * dz[k];
+        grad_E[i][j][k] -= d_v13;
+
+        // += (i, j, k+1)
+        d_v16 = V[i][j][k + 1] * psi[i][j][k + 1] * dx[i] * dy[j] * dz[k];
+        grad_E[i][j][k + 1] -= d_v16;
+
+////////////////////////////////////////////////////////////////
+        //слагаемые, входящие в производную знаменателя функционала энергии
+        // += (i, j, k)
+        d_w1 = psi[i][j][k] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+        grad_E[i][j][k] += d_w1;
+
+        // += (i, j, k+1)
+        d_w4 = psi[i][j][k + 1] * dx[i] * dy[j] * dz[k] * numerator / denumerator;
+        grad_E[i][j][k + 1] += d_w4;
     }
 
 
     for (i = 0; i < NUM; i++) {
         for (j = 0; j < NUM; j++) {
             for (k = M; k < NUM2; k++) {
-                grad_E[i][j][k] = grad_E[i][j][k] / 4. /
-                                  denumerator;
+                grad_E[i][j][k] = grad_E[i][j][k] / 4. / denumerator;
             }
         }
     }
@@ -384,13 +644,12 @@ double calc_G() {
 
     if (norm_a * norm_b == 0) {
         cos_ab = 0;
-    }
-    else {
+    } else {
         cos_ab = dot_ab / (norm_a * norm_b);
     }
     ans = (1. + 0.1 * tanh((cos_ab - 0.5) * 2.) - 0.1 * tanh((1. - 0.5) * 2.));
 
-   return ans;
+    return ans;
 }
 
 int main() {
@@ -423,9 +682,9 @@ int main() {
                 for (i = i0_x - 2; i >= 0; i--) {
                     x[i] = x[i + 1] - step1_x_left * (1 - (i - (i0_x - 2)) * step_x_left);
                 }
-            }
-            else if (i0_x > 0) { //т.е. здесь обрабатывается максимум две точки
-                x[i0_x - 1] = a / 2. - (a / 2.) / (i0_x) / 2.;   //делим пополам, чтобы наши точки были тождественны узлам решетки
+            } else if (i0_x > 0) { //т.е. здесь обрабатывается максимум две точки
+                x[i0_x - 1] = a / 2. - (a / 2.) / (i0_x) /
+                                       2.;   //делим пополам, чтобы наши точки были тождественны узлам решетки
                 for (i = i0_x - 2; i >= 0; i--) {
                     x[i] = x[i + 1] - (a / 2.) / (i0_x);
                 }
@@ -451,17 +710,15 @@ int main() {
                 z[k] = z[k + 1] - step_z;
             }
             ////////////////////////////////////////////////////////////
-            if(i0_x > 2) {
+            if (i0_x > 2) {
                 printf("%lf\t %lf\t %lf\t %lf\t %lf\t %lf\t\n", x[0], step1_x_left / 2., step1_x_right / 2.,
                        x[i0_x + 1],
                        x[NUM - i0_x - 1], x[NUM - 1]);
-            }
-            else if (i0_x > 0){
+            } else if (i0_x > 0) {
                 printf("%lf\t %lf\t %lf\t %lf\t %lf\t %lf\t\n", x[0], (a / 2.) / (i0_x) / 2., step1_x_right / 2.,
                        x[i0_x + 1],
                        x[NUM - i0_x - 1], x[NUM - 1]);
-            }
-            else{
+            } else {
                 printf("%lf\t %lf\t %lf\t %lf\t %lf\t %lf\t\n", x[0], step1_x_right / 2., x[i0_x + 1],
                        x[NUM - i0_x - 1], x[NUM - 1]);
             }
@@ -513,7 +770,7 @@ int main() {
                 calculate_grad_E();
                 G = calc_G();
 
-            normalize_to_1();
+                normalize_to_1();
 
                 max_grad_E = grad_E[0][0][M];
                 for (i = 0; i < NUM; i++) {
@@ -531,7 +788,7 @@ int main() {
                            E, E_kinetic, E_potential, max_grad_E);
                     count_prints++;
                     count_steps = 0;
-fflush(stdout);
+                    fflush(stdout);
 
                 }
                 count_steps++;
@@ -549,8 +806,8 @@ fflush(stdout);
             fprintf(f, "%.1f\t %.1f\t %.1f\t	%.12lf\t	%.12lf\t	%.12lf\t	%.12le\t\n", L, z0, a, E, E_kinetic,
                     E_potential, max_grad_E);
 
-fflush(stdout);
-fflush(f);
+            fflush(stdout);
+            fflush(f);
 
             a -= 0.5;
         }
